988bea8dcdd6f33417cb5f23a798190067d0926d
HhDreamer <hh.dreamer@gmail.com>
2023-10-17 15:53:58 +0200

    commit finish faq page.

--- src/components/DownloadLink.jsx
+++ src/components/DownloadLink.jsx
@@ -5,7 +5,7 @@ const DownloadLink = ({ convertedFile }) => {
       <a
         href={url}
         download='fichier_converti.xml'
-        className='btn btn-link text-lg normal-case'
+        className='btn btn-link text-sky-500 hover:text-sky-600 text-lg normal-case'
       >
         Télécharger le fichier converti
       </a>
