5309b4619f06b9b52905964415eeb0e31af3dfeb
HhDreamer <hh.dreamer@gmail.com>
2023-11-06 16:23:29 +0100

    commit change the penalty structure.

--- api/index.py
+++ api/index.py
@@ -91,7 +91,7 @@ def creer_question_xml(question, question_num, qcm_type, penalty):
     ET.SubElement(ET.SubElement(question_xml, 'incorrectfeedback', format='html'), 'text').text = 'Votre réponse est incorrecte.'
     ET.SubElement(question_xml, 'shownumcorrect')
 
-    # incorrect_fraction = '-50' if qcm_type == '3' else '-33.33'
+    # incorrect_fraction = '-50' if qcm_type == '3' else '-33.33333'
 
     if penalty == '0':
         incorrect_fraction = '0'
